3f9808feb90693dccabc9f2b7887813bf4153647
kungumavarsha <varshamanohar2202@gmail.com>
2025-07-14 19:08:53 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../components/Home';
 import About from '../components/about';
 import Menu from '../components/Menu';
-import Order from '../components/Order';
 
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/menu" element={<Menu />} />
-        <Route path="/Order" element={<Order />} />
+        
       </Routes>
     </Router>
   );
